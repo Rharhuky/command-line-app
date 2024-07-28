79b69342c66dc34591dd8bf1424d4940ed53eb08
Rharhuky <rharhuky@gmail.com>
2024-07-28 15:55:16 -0300

    feat: searching dns

--- app/app.go
+++ app/app.go
@@ -14,26 +14,26 @@ func Create() *cli.App {
 	application := cli.NewApp()
 	application.Name = "command line app"
 	application.Usage = "searching for IPs address and server names on the internet"
-
+	flags := defaultFlags()
 	application.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Searching Ips from internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "github.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Searching Ips from internet",
+			Flags:  flags,
 			Action: searchingIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Searching server name on the internet",
+			Flags:  flags,
+			Action: searchingServers,
 		},
 	}
 	return application
 }
 
 func searchingIps(context *cli.Context) {
-	host := context.String("host")
-
+	host := searchingHost(context)
 	ips, err := net.LookupIP(host)
 	if err != nil {
 		log.Fatal(err)
@@ -43,4 +43,30 @@ func searchingIps(context *cli.Context) {
 		fmt.Println(ip)
 	}
 
+}
+
+func searchingServers(context *cli.Context) {
+	host := searchingHost(context)
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
+	}
+
+}
+
+func defaultFlags() []cli.Flag {
+	return []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "github.com",
+		},
+	}
+}
+
+func searchingHost(context *cli.Context) string {
+	return context.String("host")
 }
